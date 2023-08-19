1eb34a263d1bd7ec7ef357ed5a2d01c6b6d0c8b2
proxict <bobogu@protonmail.com>
2023-08-19 19:25:03 +0200

    Disable semantic tokens provider

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,53 +1,15 @@
--- Setup language servers.
 local lspconfig = require('lspconfig')
-
-lspconfig.clangd.setup {}
-lspconfig.rust_analyzer.setup {
-    -- Server-specific settings. See `:help lspconfig-setup`
-    settings = {
-        ['rust-analyzer'] = {},
-    },
-}
-lspconfig.bashls.setup{}
-lspconfig.pyright.setup{}
-lspconfig.cmake.setup{}
-
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', '<Leader>w', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', '<Leader>r', vim.lsp.buf.references, opts)
-        --vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        --vim.keymap.set('n', '<space>wl', function()
-        --    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        --end, opts)
-        --vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        --vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        --vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        --vim.keymap.set('n', '<space>f', function()
-        --    vim.lsp.buf.format { async = true }
-        --end, opts)
-    end,
-})
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = false
 
-local servers = { 'clangd', 'rust_analyzer' }
+local servers = { 'clangd', 'rust_analyzer', 'bashls', 'pyright', 'cmake' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
-        -- on_attach = my_custom_on_attach,
+        on_attach = function(client)
+            -- help vim.lsp.semantic_tokens.start()
+            client.server_capabilities.semanticTokensProvider = nil
+        end,
         capabilities = capabilities,
     }
 end
@@ -143,3 +105,33 @@ vim.fn.sign_define("DiagnosticSignWarn",   {text = '',  texthl = "DiagnosticS
 vim.fn.sign_define("DiagnosticSignInfo",   {text = '',  texthl = "DiagnosticSignInfo"})
 vim.fn.sign_define("DiagnosticSignHint",   {text = '',  texthl = "DiagnosticSignHint"})
 --vim.keymap.set('n', '<Leader>e', ':lopen<CR>', { silent = true })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<Leader>w', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<Leader>r', vim.lsp.buf.references, opts)
+        --vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        --vim.keymap.set('n', '<space>wl', function()
+        --    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        --end, opts)
+        --vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        --vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        --vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        --vim.keymap.set('n', '<space>f', function()
+        --    vim.lsp.buf.format { async = true }
+        --end, opts)
+    end,
+})
+
