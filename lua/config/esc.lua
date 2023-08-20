29d481e684cd2e2f17a9f87fb3cbd7979edaacf2
proxict <bobogu@protonmail.com>
2023-08-20 15:08:52 +0200

    Don't close window with ESC when it's the last window

--- lua/config/esc.lua
+++ lua/config/esc.lua
@@ -1,9 +1,19 @@
 -- ESC in these filetypes will close them
+
+-- Maybe not even needed with no-buflisted
+function is_split_window()
+    -- If the number of windows is more than 1, we're in a split
+    return vim.fn.winnr('$') > 1
+end
+
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'qf', 'help', 'man', 'NvimTree', 'preview', 'ale-preview', 'ale-preview.message', 'ale-preview-selection' },
-    group    = vim.api.nvim_create_augroup('EscFileTypeQuit', {}),
+    group   = vim.api.nvim_create_augroup('EscFileTypeQuit', {}),
     callback = function()
-        local buf_index = vim.api.nvim_get_current_buf()
-        vim.api.nvim_buf_set_keymap(buf_index, 'n', '<Esc>', ':quit<CR>', { noremap = true, silent = true })
+        vim.opt_local.buflisted = false
+        vim.api.nvim_buf_set_keymap(0, 'n', '<Esc>',
+            ':lua if is_split_window() then vim.api.nvim_win_close(0, false) end<CR>',
+            { noremap = true, silent = true }
+        )
     end
 })
