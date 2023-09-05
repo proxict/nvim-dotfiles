d67b0e60dc5315576cf3e11a0c5dcb9fa0e0a42a
proxict <bobogu@protonmail.com>
2023-09-05 17:06:50 +0200

    Better buffer delete and alternation

--- lua/config/buffer-workflow.lua
+++ lua/config/buffer-workflow.lua
@@ -1,11 +1,21 @@
 -- Open a new empty buffer
 vim.keymap.set('n', '<Leader>T', ':enew<CR>', { noremap = false, silent = true })
 
+local function alternate_buffers()
+    vim.cmd('buffer ' .. vim.fn.bufnr('#'))
+end
+
+local function close_buffer()
+    local current_buffer_index = vim.api.nvim_get_current_buf()
+    alternate_buffers()
+    vim.cmd('bdelete ' .. current_buffer_index)
+end
+
 -- Close the current buffer and move to the previous one
-vim.keymap.set('n', '<Leader>bd', ':bw<CR>', { noremap = false, silent = true })
+vim.keymap.set('n', '<Leader>bd', close_buffer, { noremap = false, silent = true })
 
 -- Switch to a previously active buffer
-vim.keymap.set('n', '<Leader>bs', ':b#<CR>', { noremap = false, silent = true })
+vim.keymap.set('n', '<Leader>bs', alternate_buffers, { noremap = false, silent = true })
 
 -- Easier scrolling navigation and back/forward navigation
 vim.keymap.set('', '<C-j>', '<C-e>', { noremap = true, silent = true })
